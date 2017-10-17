62ad22c5f70779ff1ec4da5480027cc67c333d61
viinay <viinay@users.noreply.github.com>
2017-10-17 18:48:36 +0530

    Create working.js

---
+++ working.js
@@ -0,0 +1,65 @@
+var Imap = require('imap'),
+    inspect = require('util').inspect;
+
+var imap = new Imap({
+  user: 'testing.gopbn@gmail.com',
+  password: 'admin@12',
+  host: 'imap.gmail.com',
+  port: 993,
+  tls: true
+});
+
+function openInbox(cb) {
+  imap.openBox('INBOX', true, cb);
+}
+
+imap.once('ready', function() {
+  openInbox(function(err, box) {
+    if (err) throw err;
+
+    var f = imap.seq.fetch('1:3', {
+      bodies: '',
+      struct: true
+    });
+
+    f.on('message', function(msg, seqno) {
+      //console.log('Message #%d', seqno);
+      //var prefix = '(#' + seqno + ') ';
+
+      msg.on('body', function(stream, info) {
+        var buffer = '';
+        stream.on('data', function(chunk) {
+          buffer += chunk.toString('utf8');
+        });
+        stream.once('end', function() {
+          //console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));+
+          console.log('=======================')
+          console.log(buffer.toString())
+          console.log('=======================')
+        });
+      });
+
+      
+      msg.once('end', function() {
+        console.log('Finished');
+      });
+    });
+    f.once('error', function(err) {
+      console.log('Fetch error: ' + err);
+    });
+    f.once('end', function() {
+      console.log('Done fetching all messages!');
+      imap.end();
+    });
+  });
+});
+
+imap.once('error', function(err) {
+  console.log(err);
+});
+
+imap.once('end', function() {
+  console.log('Connection ended');
+});
+
+imap.connect();
